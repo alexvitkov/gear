1731c6dc6352c85adc3d71ee6e259af3c7ef6915
Alex Vitkov <alexvitkov98@gmail.com>
2021-10-19 16:48:59 +0300

    sova: fixed type parenting bug

--- sova/src/Type.cpp
+++ sova/src/Type.cpp
@@ -18,8 +18,11 @@ bool Type::casts_to(Type *other) {
 
 Type *register_builtin_type(GlobalContext &global, type_t id, String name, Type *parent) {
   Type *t = new Type(id, name);
+  t->parent = parent;
+
   global.types[id] = t;
   global.define(name, t);
+
   return t;
 }
 
@@ -27,29 +30,30 @@ void Type::init(GlobalContext &global) {
   global.next_type_id = TYPE_MAX_ENUM_SIZE;
   global.types.resize(TYPE_MAX_ENUM_SIZE);
 
-  register_builtin_type(global, TYPE_NIL, "Object", nullptr);
-  Type *object = register_builtin_type(global, TYPE_NIL, "Nil", nullptr);
+  Type *t_object = register_builtin_type(global, TYPE_OBJECT, "Object", nullptr);
+  register_builtin_type(global, TYPE_NIL, "Nil", nullptr);
 
-  register_builtin_type(global, TYPE_BOOL, "Bool", object);
-  register_builtin_type(global, TYPE_NUMBER, "Number", object);
-  register_builtin_type(global, TYPE_STRING, "String", object);
-  register_builtin_type(global, TYPE_CONTEXT, "Context", object);
-  register_builtin_type(global, TYPE_LIST, "List", object);
-  register_builtin_type(global, TYPE_TYPE, "Type", object);
 
-  register_builtin_type(global, TYPE_CALL, "Call", object);
-  register_builtin_type(global, TYPE_FUNCTION, "Function", object);
-  register_builtin_type(global, TYPE_FORM, "Form", object);
-  register_builtin_type(global, TYPE_AST_IF, "If", object);
-  register_builtin_type(global, TYPE_AST_WHILE, "While", object);
-  register_builtin_type(global, TYPE_AST_BLOCK, "Block", object);
-  register_builtin_type(global, TYPE_REFERENCE, "Reference", object);
-  register_builtin_type(global, TYPE_OBJECT_PTR, "ObjectPTr", object);
-  register_builtin_type(global, TYPE_CONTEXT_FIELD_ACCESSOR, "ContextFieldAccessor", object);
-  register_builtin_type(global, TYPE_LIST_ACCESSOR, "ListAccessor", object);
-  register_builtin_type(global, TYPE_UNQUOTE, "Unquote", object);
+  register_builtin_type(global, TYPE_BOOL, "Bool", t_object);
+  register_builtin_type(global, TYPE_NUMBER, "Number", t_object);
+  register_builtin_type(global, TYPE_STRING, "String", t_object);
+  register_builtin_type(global, TYPE_CONTEXT, "Context", t_object);
+  register_builtin_type(global, TYPE_LIST, "List", t_object);
+  register_builtin_type(global, TYPE_TYPE, "Type", t_object);
 
-  register_builtin_type(global, TYPE_RUNTIME_ERROR, "RuntimeError", object);
+  register_builtin_type(global, TYPE_CALL, "Call", t_object);
+  register_builtin_type(global, TYPE_FUNCTION, "Function", t_object);
+  register_builtin_type(global, TYPE_FORM, "Form", t_object);
+  register_builtin_type(global, TYPE_AST_IF, "If", t_object);
+  register_builtin_type(global, TYPE_AST_WHILE, "While", t_object);
+  register_builtin_type(global, TYPE_AST_BLOCK, "Block", t_object);
+  register_builtin_type(global, TYPE_REFERENCE, "Reference", t_object);
+  register_builtin_type(global, TYPE_OBJECT_PTR, "ObjectPTr", t_object);
+  register_builtin_type(global, TYPE_CONTEXT_FIELD_ACCESSOR, "ContextFieldAccessor", t_object);
+  register_builtin_type(global, TYPE_LIST_ACCESSOR, "ListAccessor", t_object);
+  register_builtin_type(global, TYPE_UNQUOTE, "Unquote", t_object);
+
+  register_builtin_type(global, TYPE_RUNTIME_ERROR, "RuntimeError", t_object);
 }
 
 void Type::print(Ostream &o) { o << name; }
